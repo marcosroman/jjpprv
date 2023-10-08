1104ec73e9e5d7880a556318c43577bfa70032ff
marcosroman <marcosdromand@gmail.com>
2023-10-08 16:54:26 -0300

    managed to connect to mongodb (atlas)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>ja-japo-porave</h1>
+<h2>(jjpprv)</h2>
+<h3>Glasstex</h3>
+<p>gestion de mejora continua</p>
