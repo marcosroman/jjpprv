cdc24bb91df09a74884ec23d5a10a79e86f5bd2b
marcosroman <marcosdromand@gmail.com>
2023-10-16 19:37:26 -0300

    moved corrective actions subform to a separate component

--- src/routes/capa/respond/[id]/+page.svelte
+++ src/routes/capa/respond/[id]/+page.svelte
@@ -1,25 +1,14 @@
 <script>
+	import CorrectiveActionsSubForm from './CorrectiveActionsSubForm.svelte';
+
 	export let data;
 
-	let isCorrectiveActionRequired=false; //set to false later
+	let isCorrectiveActionRequired = false; //set to false later
 	let countCorrectiveActions = Number(isCorrectiveActionRequired);
-	let correctiveActionsArray = [{solution: "", commitmentDate: ""}];
 
-	function addNewCorrectiveAction() {
-		correctiveActionsArray = [
-			...correctiveActionsArray,
-			{solution: "", commitmentDate: ""}];
-		countCorrectiveActions++;
-	}
-	function deleteCorrectiveAction(id) {
-		correctiveActionsArray.splice(id,1);
-		correctiveActionsArray=correctiveActionsArray;
-		countCorrectiveActions--;
-	}
 	function updateCountCorrectiveActions() {
 		if (isCorrectiveActionRequired && countCorrectiveActions==0) {
-			countCorrectiveActions++;
-			countCorrectiveActions=countCorrectiveActions;
+			countCorrectiveActions = 1;
 		}
 	}
 </script>
@@ -32,13 +21,13 @@
 		<input type="hidden" name="responder-user-id" value="1">
 
 		<label>Acciones inmediatas:
-		<textarea name="immediate-actions" required placeholder="Acciones Inmediatas"></textarea>
+			<textarea name="immediate-actions" required placeholder="Acciones Inmediatas"></textarea>
 		</label>
 		
 		<p>evidencia...?</p>
 		
 		<label>Consecuencias:
-		<textarea name="consequences" required placeholder="Consecuencias (blabla)"></textarea>
+			<textarea name="consequences" required placeholder="Consecuencias (blabla)"></textarea>
 		</label>
 
 		<label>Requiere Accion Correctiva?
@@ -49,45 +38,10 @@
 				on:change={updateCountCorrectiveActions}>
 		</label>
 
-		<p>(DEBUG) corrective action count = {countCorrectiveActions}</p>
 		{#if isCorrectiveActionRequired}
-			<label>Causas posibles:
-				<textarea name="possible-root-causes" required></textarea>
-			</label>
-
-			<input type="hidden" name="count-corrective-actions" value={countCorrectiveActions}>
-			<table>
-				<tr>
-					<th>Solucion</th>
-					<th>Fecha de compromiso</th>
-				</tr>
-
-				{#each correctiveActionsArray as correctiveAction, i}
-					<tr>
-						<td>
-							<input
-								type="text"
-								name={"solution-"+i}
-								bind:value={correctiveAction.solution} 
-								placeholder={i}
-								required>
-						</td>
-						<td>
-							<input
-								type="date"
-								name={"commitment-date-"+i}
-								bind:value={correctiveAction.commitmentDate}
-								required>
-						</td>
-						{#if countCorrectiveActions>1}
-						<td>
-							<button name={"delete-"+i} on:click|preventDefault={() => deleteCorrectiveAction(i)}>Eliminar</button>
-						</td>
-						{/if}
-					</tr>
-				{/each}
-			</table>
-			<button on:click|preventDefault={addNewCorrectiveAction}>Agregar otra</button>
+			<CorrectiveActionsSubForm 
+				{countCorrectiveActions}
+				{isCorrectiveActionRequired}/>
 		{/if}
 
 		<input type="submit" value="Guardar">
