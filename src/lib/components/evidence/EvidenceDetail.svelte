438d3ae472a16270667e57f48fefb39c1e2b1d27
marcosroman <marcosdromand@gmail.com>
2023-10-24 16:10:37 -0300

    EvidenceDetail now showing basic evidence details

--- src/lib/components/evidence/EvidenceDetail.svelte
+++ src/lib/components/evidence/EvidenceDetail.svelte
@@ -7,13 +7,29 @@
 
 	onMount(async () => {
 		evidenceData = await getEvidenceDataFromEvidence(evidenceId);
+		console.log(evidenceData.fileName);
 	});
+
+	function openWindow() {
+		const base64String = evidenceData.fileData;
+		const base64Data = base64String.split(',')[1];
+		const arrayBuffer = Uint8Array.from(
+			atob(base64Data),
+			c => c.charCodeAt(0)).buffer;
+
+		const blob = new Blob([arrayBuffer],{ type: evidenceData.fileType });
+
+		const url = URL.createObjectURL(blob, evidenceData.fileName);
+		window.open(url);
+		URL.revokeObjectURL(url);
+	}
 </script>
 
 <p>{evidenceId}</p>
-<p>{JSON.stringify(evidenceData)}</p>
-<!--
-<p>{evidenceData.fileName}</p>
-<p>{evidenceData.fileType}</p>
--->
-
+{#if evidenceData}
+	<p>{evidenceData.fileType}</p>
+	<p>{evidenceData.fileName}</p>
+	<p>name={evidenceData.fileName}, DESC= {evidenceData.description}, TYPE={evidenceData.fileType}</p>
+	<iframe src={evidenceData.fileData} title="desc" alt='pdf'></iframe>
+	<button on:click={openWindow}>Ver</button>
+{/if}
