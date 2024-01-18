5f5132bd82484f6ad4507cb6302c56829db142c5
marcosroman <marcosdromand@gmail.com>
2024-01-18 15:52:07 -0300

    deleted "scaffolding" (links) in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,5 @@
-<style>
-	a {
-		margin:1em;
-		border: solid 1pt;
-		text-decoration: none;
-		color: red;
-	}
-	
-	.done {
-		color: darkgreen;
-	}
-
-	.done-for-capa-id {
-		color: darkblue;
-	}
-</style>
-
-
 <script>
 	import '../app.pcss';
-	//import '$lib/style.css';
 	import { onMount } from 'svelte';
 	//import { redirect } from '@sveltejs/kit';
 	import { goto } from '$app/navigation';
@@ -85,39 +66,16 @@
 		{/if}
 	{/if}
 
-	<!--
-	<label>Fecha-hora:
-		<input type='datetime-local' name="date-time" value={dateTime}>
-		<button on:click={()=>{dateTime = (new Date()).toISOString().split('Z')[0]}}>Ahora</button>
-	</label>
-	-->
-
 	<br>
 	<label>Fecha:
 		<input type='datetime-local' name="date-time" bind:value={$selectedDate}>
-		<!--<button>Ajustar fecha-hora</button>-->
 		<button on:click={()=>{$selectedDate = (new Date()).toISOString().split('Z')[0]}}>Volver al presente</button>
 	</label>
 
 	{#if currentUser}
 		<hr>
-			<a class="done" href="/">Inicio</a>
-			<a class="done" href="/capa/new">Nueva NC/OM</a>
-			<a class="done" href="/capa/new/evidence">Agregar evidencia a Nueva NC/OM [opcional?]</a>
-			<a class="done" href="/capa/respond-nc">Responder a No-Conf [solo si NC/OM es NC]</a>
-			<a class="done" href="/capa/respond-nc/evidence">Agregar Evidencia en Respuesta a No-Conf. (solo si capa es NC)</a>
-			<a class="done" href="/capa/decide-ca">Decidir si requiere Acciones Correctivas (CSGC)</a>
-			<a class="done" href="/capa/act/propose">Analizar (si es NC) y Proponer Acciones ante NC/OM (para NC solo si requiere ACs)</a>
-			<a class="done" href="/capa/act/assign">Asignar Responsable ante NC/OM (para NC solo si requiere ACs)</a>
-			<a class="done" href="/capa/act/accept">Aceptar Responsabilidad ante NC/OM (para NC solo si requiere ACs)</a>
-			<a class="done" href="/capa/act/evidence">Subir evidencia de acciones ante NC/OM (para NC solo si requiere ACs)</a>
-			<a class="done-for-capa-id" href="/capa/act/reschedule">Reagendar accion (para NC solo si requiere ACs)</a>
-			<!--<a disabled href="/capa/act/reschedule/assign">Asignar accion reagendada (para NC solo si requiere ACs)</a>-->
-			<a class="done-for-capa-id" href="/capa/act/reschedule/accept">Aceptar accion reagendada (para NC solo si requiere ACs)</a>
-			<a class="done" href="/capa/act/review">Hacer seguimiento de Acciones (CSGC)</a>
-			<a class="done" href="/capa/evaluate/assign">Asignar Evaluador para NC/OM (CSGC)</a>
-			<a class="done" href="/capa/evaluate">Evaluar NC/OM</a>
-			<a class="done" href="/capa/close">Cerrar NC/OM (CSGC)</a>
+			<a href="/">Inicio</a>
+			<a href="/capa/new">Nueva NC/OM</a>
 			<a href="/capa/view">Ver NC/OM</a>
 			{#if currentUser.isQMSStaff}
 				<a href="/capa/view/all">Ver todas las NC/OM (CSGC)</a>
@@ -125,7 +83,6 @@
 		<hr>
 	{/if}
 </nav>
-
 
 <slot></slot>
 
