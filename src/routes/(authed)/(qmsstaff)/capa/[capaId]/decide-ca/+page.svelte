0547ef7786346ada44be22de51fca4661df6ca75
marcosroman <marcosdromand@gmail.com>
2024-01-28 17:33:23 -0300

    styled

--- src/routes/(authed)/(qmsstaff)/capa/[capaId]/decide-ca/+page.svelte
+++ src/routes/(authed)/(qmsstaff)/capa/[capaId]/decide-ca/+page.svelte
@@ -7,20 +7,26 @@
 	let capaId = capa._id;
 </script>
 
-<form method='POST'>
+
+<CapaView {capaId}/>
+<hr>
+
+<form method='POST' class="flex align-center flex-col">
 	<input type="hidden" name="capa-id" value={capaId}>
 
-	<p>Requiere accion correctiva?</p>
-	<label>Si
-		<input type="radio" name="is-ca-required" value="true" required>
-	</label>
-	
-	<label>No
-		<input type="radio" name="is-ca-required" value="false" required>
-	</label>
+	<p class="text-center">¿Requiere acciones correctivas?</p>
+	<div class="flex flex-row justify-evenly my-4">
+		<label>Sí
+			<input type="radio" name="is-ca-required" value="true" required>
+		</label>
+		
+		<label>No
+			<input type="radio" name="is-ca-required" value="false" required>
+		</label>
+	</div>
 
-	<input type="submit" value="Guardar">
+	<div class="flex flex-row justify-center my-10">
+		<input type="submit" value="Guardar">
+	</div>
 </form>
 
-<hr>
-<CapaView {capaId}/>
--- src/routes/(authed)/capa/[capaId]/respond-nc/+page.svelte
+++ src/routes/(authed)/capa/[capaId]/respond-nc/+page.svelte
@@ -1,21 +1,27 @@
 <script>
 	import CapaView from '$lib/components/capa/CapaView.svelte';
-	import EvidenceList from '$lib/components/evidence/EvidenceList.svelte';
 
 	export let data;
 
 	let capaId = data.capa._id;
 </script>
 
-<form method="POST">
+<style>
+	label, textarea {
+		margin: 1vh;
+	}
+</style>
+
+<form method="POST" class="container flex-col">
 	<input type="hidden" name="id" value={capaId}>
 
-	<label>Consecuencias:
-		<textarea name="consequences" required placeholder="Consecuencias (blabla)"></textarea>
+	<label>Acciones inmediatas
+		<textarea class="w-full min-h-30 h-40 my-2" name="proposed-immediate-solution" required placeholder="Para controlar o corregir la No-Conformidad"></textarea>
 	</label>
 
-	<label>Acciones inmediatas:
-		<textarea name="proposed-immediate-solution" required placeholder="Acciones Inmediatas"></textarea>
+
+	<label>Consecuencias de la No-Conformidad
+		<textarea class="w-full min-h-30 h-40 my-2" name="consequences" required placeholder="En caso de que existan"></textarea>
 	</label>
 
 	<input type="submit" value="Guardar">
