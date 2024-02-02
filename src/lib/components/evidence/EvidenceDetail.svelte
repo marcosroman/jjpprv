0c5748f7e13de203c19bf1f71814acff5c191ea7
marcosroman <marcosdromand@gmail.com>
2024-02-02 10:52:35 -0300

    evidence detail shown now as a card on top, prettier

--- src/lib/components/evidence/EvidenceDetail.svelte
+++ src/lib/components/evidence/EvidenceDetail.svelte
@@ -3,6 +3,7 @@
 	import { getEvidenceDataFromEvidence } from './handleEvidenceData.js';
 
 	export let evidenceId;
+	export let hideEvidenceDetail;
 	let evidenceData;
 
 	onMount(async () => {
@@ -25,11 +26,15 @@
 	}
 </script>
 
-<p>{evidenceId}</p>
 {#if evidenceData}
-	<p>{evidenceData.fileType}</p>
-	<p>{evidenceData.fileName}</p>
-	<p>name={evidenceData.fileName}, DESC= {evidenceData.description}, TYPE={evidenceData.fileType}</p>
-	<iframe src={evidenceData.fileData} title="desc" alt='pdf'></iframe>
-	<button on:click={openWindow}>Ver</button>
+	<div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 opacity-100">
+		<!--<div class="absolute inset-0 bg-gray-800 opacity-50">-->
+    <div class="flex inset-0 p-6 w-3/4">
+    	<div class="z-50 w-full bg-white p-6 rounded shadow-md flex flex-col items-center justify-center opacity-100">
+				<iframe class="opacity-100 w-3/4 my-4 p-4 border-solid border-4 border-black" src={evidenceData.fileData} title={evidenceData.fileName} alt="evidence"></iframe>
+				<button class="opacity-100 my-2" on:click={openWindow}>Abrir en <wbr>otra ventana</button>
+				<button class="opacity-100 bg-gray-900 my-2" on:click={hideEvidenceDetail}>Cerrar</button>
+			</div>
+    </div>
+  </div>
 {/if}
