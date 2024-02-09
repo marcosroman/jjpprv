562f054b157570e783fa33193743aac55166c0c3
marcosroman <marcosdromand@gmail.com>
2024-02-09 09:28:24 -0300

    improved styling a bit more

--- src/lib/components/evidence/EvidenceDetail.svelte
+++ src/lib/components/evidence/EvidenceDetail.svelte
@@ -29,13 +29,16 @@
 
 {#if evidenceData}
 	<div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 opacity-100">
-    <div class="flex inset-0 p-6 w-3/4">
-    	<div class="z-50 w-full bg-white p-6 rounded shadow-md flex flex-col items-center justify-center opacity-100">
-				{#if evidenceData.fileType.split('/')[0] === 'image'}
-					<img class="h-1/3 w-3/4 my-4 p-4 border-solid border-4 border-black" src={evidenceData.fileData}>
-				{:else}
-					<iframe class="opacity-100 h-3/4 w-3/4 my-4 p-4 border-solid border-4 border-black" src={evidenceData.fileData} title={evidenceData.fileName} alt="evidence"></iframe>
-				{/if}
+    <div class="flex inset-0 h-3/4 w-1/2">
+    	<div class="z-50 w-full bg-white rounded shadow-md flex flex-col items-center justify-center opacity-100">
+					<!--"aspect-auto h-1/3 w-3/4 my-4 p-4 border-solid border-black"-->
+					{#if evidenceData.fileType.split('/')[0] === 'image'}
+						<div class="h-full w-1/2 my-4 p-4 border-2 rounded border-solid border-black flex justify-center align-center">
+							<img alt="evidence" class="" src={evidenceData.fileData}>
+						</div>
+					{:else}
+						<iframe class="opacity-100 h-full w-1/2 my-4 p-4 border-solid border-4 border-black" src={evidenceData.fileData} title={evidenceData.fileName} alt="evidence"></iframe>
+					{/if}
 				<p class="p-4 m-4">{evidenceData.description}</p>
 				<div class="flex flex-row flex-wrap justify-evenly p-4 w-1/2">
 					<button class="opacity-100 my-2" on:click={openWindow}>Abrir en <wbr>otra ventana</button>
