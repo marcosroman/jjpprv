064e39682c2d06fd253235f4efa0811da6f7665e
marcosroman <marcosdromand@gmail.com>
2024-02-12 15:45:28 -0300

    also complained after build, so did the same trick (see pkg variable)

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,20 +1,21 @@
 import { start_mongo } from '$lib/db/mongo';
 import { SECRET_KEY } from '$env/static/private';
-import { verify } from 'jsonwebtoken';
+import { handlePageLoad } from '$lib/db/mongo';
+import pkg from 'jsonwebtoken';
 
-start_mongo().then(() => {
-	console.log('mongo started!');
-}).catch(console.log);
+start_mongo();
 
 export async function handle({ event, resolve }) {
-	console.log('handle running!');
+	const { verify } = pkg;
 
 	const userToken = event.cookies.get('userToken');
-	if(userToken) {
+
+	if (userToken) {
 		event.locals.user = verify(userToken, SECRET_KEY).user;
 	}
 
 	const response = await resolve(event);
+
 	return response;
 }
 
