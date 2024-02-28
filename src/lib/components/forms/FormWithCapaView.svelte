92e4d513e3e8410f5065df46e6b76a14a38b5411
marcosroman <marcosdromand@gmail.com>
2024-02-27 23:32:08 -0300

    improved widths a bit

--- src/lib/components/forms/FormWithCapaView.svelte
+++ src/lib/components/forms/FormWithCapaView.svelte
@@ -10,7 +10,7 @@
 
 <div class="w-full md:h-screen h-max">
 	<div class="flex flex-col w-full md:h-dvh h-max">
-		<div class="flex flex-row justify-center p-8">
+		<div class="flex flex-row justify-center p-1">
 			<button on:click|preventDefault={() =>
 				{isCapaViewShown = !isCapaViewShown;}}>
 				{(isCapaViewShown ? "Ocultar" : "Mostrar")+" registro"}
@@ -18,7 +18,7 @@
 		</div>
 
 		<div class="flex flex-row flex-wrap self-center overflow-scroll
-			justify-evenly align-center p-6 m-6 h-2/3 w-full">
+			justify-evenly align-center p-2 m-2 h-2/3 w-full">
 			{#if isCapaViewShown}
 				<div class="m-4 h-1/3 w-full md:w-1/3 md:h-5/6
 					border-solid border-black border-2 overflow-scroll">
@@ -32,7 +32,7 @@
 				</div>
 			{/if}
 
-			<div class={"w-3/4 m-6 md:w-1/3"+" h-1/3 overflow-auto md:h-5/6"}>
+			<div class={(isCapaViewShown ? "w-3/4 md:w-1/2 " : "w-full")+" m-6 "+"h-1/3 overflow-auto md:h-5/6"}>
 				<Form {...formProps}/>
 			</div>
 		</div>
